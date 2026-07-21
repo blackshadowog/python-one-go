d7151b40c8a77f4707b27f59f929b9950bbc9088
blackshadowog <atiwari73874@gmail.com>
2026-07-21 22:35:58 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,5 @@
+print('''Twinkle, twinkle, little star, how I wonder what you are”
+“Up above the world so high, like a diamond in the sky”
+“When the blazing sun is set, and the grass with dew is wet”
+“Then you show your little light, twinkle, twinkle all the night”
+“Then the traveler in the dark thanks you for your tiny spark''')
