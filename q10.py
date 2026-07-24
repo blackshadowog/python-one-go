9c29c8b50e462e70392b24f22e660cf28431076f
blackshadowog <atiwari73874@gmail.com>
2026-07-24 22:55:21 +0530

    Add files via upload

---
+++ q10.py
@@ -0,0 +1,4 @@
+name=input("enter your name")
+print("gm ,", name)
+
+print(f"gm ,{name}")
---
+++ q9.py
@@ -0,0 +1,11 @@
+a = int(input("Enter number 1: "))
+
+
+print("square is", a*a)
+
+# or 
+
+a = int(input("Enter number 1: "))
+
+
+print("square is", a**2)
