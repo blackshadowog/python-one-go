257a63c46a3ad925f60104cbdd08e10b89f087be
blackshadowog <atiwari73874@gmail.com>
2026-09-10 23:30:23 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,4 @@
+music = { "Sky" : "https://youtu.be/sZrTJesvJeo?si=5tSA4EUt6J5rwWk3",
+         "NEON " : "https://youtu.be/BW13UjuZ_qc?si=39qm6WqNRoxlbq79",
+         "INTRO": "https://youtu.be/VhsbZzwCzx8?si=mAn3CSTLBrHAYFyB",
+         "RAMAYANA" : "https://youtu.be/DaS1kllkk-4?si=swTOMV97bgH0M2_e"}
